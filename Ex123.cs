5210d9cf7327c581c0a021fdb00a9dd90b7081c4
Humberto <humbertoconstantino18@gmail.com>
2022-07-20 09:46:58 -0300

    1ex

---
+++ Ex123.cs
@@ -0,0 +1,24 @@
+﻿//EX 1
+
+double largura = Double.Parse(Console.ReadLine());
+double comprimento = Double.Parse(Console.ReadLine());
+
+double area = largura * comprimento;
+
+Console.WriteLine("O TERRENO TEM " + area + " METROS QUADRADOS");
+
+//EX2
+
+int quantidade_cavalos = int.Parse(Console.ReadLine());
+int ferraduras = quantidade_cavalos * 4;
+Console.WriteLine("Para equipar "+quantidade_cavalos+" cavalos, são necessários "+ ferraduras +" ferraduras");
+
+//EX3
+
+int idade = int.Parse(Console.ReadLine());
+int dias = idade * 365;
+
+Console.WriteLine("Você já viveu "+ dias + " dias");
+
+//Humberto Constantino de Andrade Silva Neto
+
