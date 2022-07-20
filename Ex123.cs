b842cb54a63a980deeca5baf946e5d216467a6d7
Humberto <humbertoconstantino18@gmail.com>
2022-07-20 09:51:19 -0300

    Resolução dos Exercícios 1 2 3

--- Ex123.cs
+++ Ex123.cs
@@ -1,4 +1,4 @@
-﻿//EX 1
+﻿///EX 1
 
 double largura = Double.Parse(Console.ReadLine());
 double comprimento = Double.Parse(Console.ReadLine());
@@ -7,13 +7,13 @@ double area = largura * comprimento;
 
 Console.WriteLine("O TERRENO TEM " + area + " METROS QUADRADOS");
 
-//EX2
+///EX2
 
 int quantidade_cavalos = int.Parse(Console.ReadLine());
 int ferraduras = quantidade_cavalos * 4;
 Console.WriteLine("Para equipar "+quantidade_cavalos+" cavalos, são necessários "+ ferraduras +" ferraduras");
 
-//EX3
+///EX3
 
 int idade = int.Parse(Console.ReadLine());
 int dias = idade * 365;
